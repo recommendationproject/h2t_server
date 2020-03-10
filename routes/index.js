89b6e02201b09d74ee23b93d7077d010342595ce
quocvc <quocvc95@gmail.com>
2020-03-10 23:05:54 +0700

    init

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,16 @@ const config = require('../utils/config');
 const bcrypt = require('bcrypt');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  let rs =await dbs.execute(`SELECT p.id, p.name, p.price, i.images FROM product p, images i where i.product_id = p.id group by i.product_id having min(i.id) order by p.id desc limit 6`,[]);
+  res.json(rs);
+});
+
+/* GET home page. */
+router.get('/product', async (req, res, next) => {
+  let product =await dbs.execute(`SELECT * FROM product where id = ?`,[req.headers.id]);
+  let images =await dbs.execute(`SELECT images 'original', images 'thumbnail' FROM images where product_id = ?`,[req.headers.id]);
+  res.json({product: product[0], images: images});
 });
 
 router.post('/signin', async function (req, res) {
