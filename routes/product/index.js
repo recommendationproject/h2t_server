6e2f4458bc6782b1a958695c586e5eb6a295ebdc
quoc95 <quoc95pro@gmail.com>
2020-06-13 17:24:44 +0700

    i

--- routes/product/index.js
+++ routes/product/index.js
@@ -13,10 +13,10 @@ const privateRouteProduct = require('./productPrivate');
 router.get('/', async (req, res, next) => {
   let rsRecommend = [];
   if(req.headers.userid){
-     rsRecommend =await dbs.execute(`SELECT p.id, p.name, p.price, i.images FROM product p, images i where i.product_id = p.id and p.status=1 and p.id in (select product_id from recommend_product where user_id = ? order by val desc) group by i.product_id having min(i.id) order by p.id desc limit 6`,[req.headers.userid]);
+     rsRecommend =await dbs.execute(`SELECT p.id, p.name, p.price, i.images FROM product p, images i where i.product_id = p.id and p.status=1 and p.id in (select product_id from recommend_product where user_id = ? order by val desc) group by i.product_id having min(i.id) order by p.id desc limit 8`,[req.headers.userid]);
   }
-  let rsNew =await dbs.execute(`SELECT p.id, p.name, p.price, i.images FROM product p, images i where i.product_id = p.id and p.status=1 group by i.product_id having min(i.id) order by p.id desc limit 6`,[]);
-  let rsHot =await dbs.execute(`SELECT p.id, p.name, p.price, i.images FROM product p, images i where i.product_id = p.id and p.status=1 and p.id in (SELECT product_id from order_detail group by product_id order by COUNT(*) desc) group by i.product_id having min(i.id) limit 6`,[]);
+  let rsNew =await dbs.execute(`SELECT p.id, p.name, p.price, i.images FROM product p, images i where i.product_id = p.id and p.status=1 group by i.product_id having min(i.id) order by p.id desc limit 8`,[]);
+  let rsHot =await dbs.execute(`SELECT p.id, p.name, p.price, i.images FROM product p, images i where i.product_id = p.id and p.status=1 and p.id in (SELECT product_id from order_detail group by product_id order by COUNT(*) desc) group by i.product_id having min(i.id) limit 8`,[]);
   res.json({new: rsNew, hot: rsHot, recommend: rsRecommend});
 });
 
